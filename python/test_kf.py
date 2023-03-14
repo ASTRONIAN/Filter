4fe147fc1177f6f121e97ccacbb90caf03db8084
Subramanian <asubramanianase@gmail.com>
2023-03-14 17:00:44 +0530

    Version 2 - C++ skeleton code

--- python/test_kf.py
+++ python/test_kf.py
@@ -27,7 +27,7 @@ class TestKF(unittest.TestCase):
         x = 0.25
         v = 2.3
 
-        kf = self.KF(initial_X=x,initial_v=v,accel_variance=1.2)
+        kf = self.KF(x,v,1.2)
         self.assertAlmostEqual(kf.pos,x)
         self.assertAlmostEqual(kf.vel,v)
 
@@ -35,7 +35,7 @@ class TestKF(unittest.TestCase):
         x = 0.25
         v = 2.3
 
-        kf = self.KF(initial_X=x,initial_v=v,accel_variance=1.2)
+        kf = self.KF(x,v,1.2)
         kf.predict(dt=0.1)
 
         self.assertEqual(kf.cov.shape,(2,2))
@@ -45,7 +45,7 @@ class TestKF(unittest.TestCase):
         x = 0.25
         v = 2.3
 
-        kf = self.KF(initial_X=x,initial_v=v,accel_variance=1.2)
+        kf = self.KF(x,v,1.2)
 
         for i in range(10):
             det_before = np.linalg.det(kf.cov)
@@ -59,17 +59,17 @@ class TestKF(unittest.TestCase):
         x = 0.25
         v = 2.3
 
-        kf = self.KF(initial_X=x,initial_v=v,accel_variance=1.2)
-        kf.update(meas_value=0.1,meas_variance=0.1)
+        kf = self.KF(x,v,1.2)
+        kf.update(0.1,0.1)
 
     def test_calling_predict_increase_state_uncertainty(self):
         x = 0.25
         v = 2.3
 
-        kf = self.KF(initial_X=x,initial_v=v,accel_variance=1.2)
+        kf = self.KF(x,v,1.2)
 
         det_before = np.linalg.det(kf.cov)
-        kf.update(meas_value=0.1,meas_variance=0.1)
+        kf.update(0.1,0.1)
         det_after = np.linalg.det(kf.cov)
 
         self.assertLess(det_after,det_before)
